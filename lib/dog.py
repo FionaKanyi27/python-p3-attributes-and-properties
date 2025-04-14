bd30dedf895ef854dc61f9c855b2142147a373e5
Fiona <fionakanyi0@gmail.com>
2025-04-14 19:10:53 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,17 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self, name="Dog", breed="Beagle"):
+        self.name = name
+        self.breed = breed
+
+        @property
+        def name(self):
+            return self._name
+        
+        @name.setter
+        def name(self, value):
+            if isinstance(value, str) and 1 <= len(value) <= 20:
+                raise TypeError("Name must be a string")
+            self._name = value
     pass
